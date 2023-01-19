06bbf1fe1c0cea278ef32a1d117df84c32617fbe
Rod Kisten <rod@Rods-MacBook-Pro.local>
2023-01-19 16:28:08 -0300

    feat: add my craft

--- blog.config.js
+++ blog.config.js
@@ -44,7 +44,7 @@ const BLOG = {
   telegramChatId: '263895784', // The chat id of your Telegram bot
   telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
   telegramChannelName: 'rodkisten', // The name of your Telegram channel
-  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/hBE96MwbY0r7aH', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
     ackeeConfig: {
