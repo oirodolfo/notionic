d7ec7fe35eb764f58f16b5937ab50b352235d727
Rod Kisten <rod@Rods-MacBook-Pro.local>
2023-02-01 13:49:44 -0300

    feat: og now acccept images

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,6 @@
 // https://og-playground.vercel.app/?share=pVVhb9owEP0rlqWKVkqJw0rbReukrau6immVBhJfkJCJTXBxbMtxgAzx33dOmkGBrlKHROK8u7w7v7tz1jjRjOMYf2JiMVII5a6U_Ga99muEZlykMxejVkTISSuowaVgbraHMZEbSUtAp5KvGtTxlfsiRaoAT7hy3DYW6tEHx7P8wPRU5E5My1sNoPKxX5o9_zdheeKErni1LDK1ax1aasCg9BIWjWFCk3lqdaHYrZbagt2mE3ra6XaD5o_aH88O3R8ymnLvTpmg8jz1d0joNBE2kRxRhzpds6ouATKFNQB2TgLkLFW5oRZ8ETk5C9DrBFeewF8CBLLMHLiUxzgOk-uL3z43qAQwVNfaZ7MZqc9-0ZR1v7CvVeyflXmzNj4sQlVgCC2ytHkvt8nNCM-cM3kchpbn7UTqgglFbdlOdBZqYTXTcqpD4fUOCyM1ZeEiurzqkqhzEXXCH4_3j-PeuPfQH9z9HM9LUhrXzhfpCDdR6m69WUfXpErE_xaCL7_qFUQniIDI6LK7fWEqpATLRIKgW7RRCmXUpsL3WMfLewEFaj1vEaGwljcEfd8p9BRErOt3SV6Aw2borgjZc_fUYJhoybYWmICqoattbOEm-y25FIp_fyaP_qJhiJYz4Xjf0MSTG8vPdycHmIQaHpn5etrurV7u0u21yAjXLTLCxzrsiFVyB4BPRqgUIp6TNrmOeNY60mK_NEM9kUOg3WL8bzk-bBVrpK3nel_bgYZz5i1536fRngrRkc3DwMyrvfsBQrsCNHccYG38MZnjeI2rYxvH14QEuJ4UHF_4B8YnRYpjZwseYJ7pJzEojf8muGX1BDRenLtswlntttn8AQ
 // v2: https://og-playground.vercel.app/?share=nVb_T-IwFP9X6hIDJoNtnJxIzkuM53lE70zUxF9IsGyPUe3W2XZ8kfC_3-sGCBvTyz3CRvs-ff207xsLyxcBWF2LfAvYpB-TjSg953C2WGzPETIGFo51l9Q81z2s2bvKKQv0uEIXMJVwOkftiMOsqNUw0-echTHqfYg1yCIiEYppJgxAAqeaTaAIocZAT0OkVrs0lKZSF2HPqdJsNL8QuE1sjlINNZofTIK_2tkXPI3ifahHSRMExGKKP0oAxvFEXdK3FNWppBrqXrN91LcKuCH1X0Ip0ji4EFzggpoMh7Tearft9Zc0T4-K5rcX3rM3qPDANqwX0dDgUsnrY60T1XUcCarpc5EGLKZy3vRF5AgmRSD4SDjMLHDShAsaOBPv60nb63TarusoiNgIV_BB_MZfZdBM4rDE0HFKe5cABE9LA0Z5IzRvdE3dZ9LnQKgmrXYyyx42SVKZ4GTr0CZa0lglVCKWuIdHNqk2cGIMmIdNMErGGiHzfTY-ZP5-uWgse9a20cvl--j7tqKYWlXJ9VmW_HOIfxzku9ht1gXeyJxFYdG0kv5Z7T-DxvVax17Lubm9uh1cD6579w-XfwYvc3ee6KaahAVq63JztvA67g7N7TskEZUhM-nZMn45RifXCqdCP-46xEGP4Ez-2bFa9tVH_solJ3ADI7zoL27JF5tIynF3eQU9cauQEZ095pW0bx13DktlYi3bRZPj5uUoyOWzoMplhKGSB7hXeQaD-UkjxtHY03msRWyTWvYmdxCmnGLZJrX7hPpArqTQoF5womfquU0UJlpDgWSjpwrzHDQizXIWh0i34TbdNkRVjPFGM9LGN1lbWDHYj8banZXU6ZjpUuvYMGAx_Fq3uNN9NXSzdWYnOypCEwmNfZXfSDEUCylm5Dek2OAgooZAIMgrSYHgHI2ETWKhFD04OChk5iqE32d2g3l3lKMt2xKJaWXK6i6srFtbXe-05dpWnmg4cjs4CmCYhlZ3RLkC24JIPLOHeWL-JuhpNkJL5uovoyEEVlfLFJbLvw
-
+// v3 https://og-playground.vercel.app/?share=nVb_T-IwFP9X6hIDJoNtnJxIzkuM53lE70zUxF9IsGyPUe3W2XZ8kfC_3-sGCBvTyz3CRvs-ff207xsLyxcBWF2LfAvYpB-TjSg953C2WGzPETIGFo51l9Q81z2s2bvKKQv0uEIXMJVwOkftiMOsqNUw0-echTHqfYg1yCIiEYppJgxAAqeaTaAIocZAT0OkVrs0lKZSF2HPqdJsNL8QuE1sjlINNZofTIK_2tkXPI3ifahHSRMExGKKP0oAxvFEXdK3FNWppBrqXrN91LcKuCH1X0Ip0ji4EFzggpoMh7Tearft9Zc0T4-K5rcX3rM3qPDANqwX0dDgUsnrY60T1XUcCarpc5EGLKZy3vRF5AgmRSD4SDjMLHDShAsaOBPv60nb63TarusoiNgIV_BB_MZfZdBM4rDE0HFKe5cABE9LA0Z5IzRvdE3dZ9LnQKgmrXYyyx42SVKZ4GTr0CZa0lglVCKWuIdHNqk2cGIMmIdNMErGGiHzfTY-ZP5-uWgse9a20cvl--j7tqKYWlXJ9VmW_HOIfxzku9ht1gXeyJxFYdG0kv5Z7T-DxvVax17Lubm9uh1cD6579w-XfwYvc3ee6KaahAVq63JztvA67g7N7TskEZUhM-nZMn45RifXCqdCP-46xEGP4Ez-2bFa9tVH_solJ3ADI7zoL27JF5tIynF3eQU9cauQEZ095pW0bx13DktlYi3bRZPj5uUoyOWzoMplhKGSB7hXeQaD-UkjxtHY03msRWyTWvYmdxCmnGLZJrX7hPpArqTQoF5womfquU0UJlpDgWSjpwrzHDQizXIWh0i34TbdNkRVjPFGM9LGN1lbWDHYj8banZXU6ZjpUuvYMGAx_Fq3uNN9NXSzdWYnOypCEwmNfZXfSDEUCylm5Dek2OAgooZAIMgrSYHgHI2ETWKhFD04OChk5iqE32d2g3l3lKMt2xKJaWXK6i6srFtbXe-05dpWnmg4cjs4CmCYhlZ3RLkC24JIPLOHeWL-JuhpNkJL5uovoyEEVlfLFJbLvw
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 
@@ -24,6 +24,12 @@ export default async function handler(req: NextRequest) {
       : 'rodkisten.com'
 
       const fontData = await font;
+     let image = null
+
+    if(searchParams.has('image')) {
+        image = searchParams.get('image')
+    }
+
 
     return new ImageResponse(
       (
@@ -85,6 +91,13 @@ export default async function handler(req: NextRequest) {
 
 
 
+
+              <img src={ image || 'https://images.unsplash.com/photo-1496449903678-68ddcb189a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+                   style={{ width: '761px',
+                       position: 'absolute',
+                       height: '761px',
+                       left: '1007px',
+                       top: '156px'}} />
 
 
 
