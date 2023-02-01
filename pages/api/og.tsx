681697b1560eca5733b2819394095fc46281ffc7
Rod Kisten <rod@Rods-MacBook-Pro.local>
2023-02-01 13:54:41 -0300

    feat: og now acccept images

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,11 +1,11 @@
 // https://og-playground.vercel.app/?share=pVVhb9owEP0rlqWKVkqJw0rbReukrau6immVBhJfkJCJTXBxbMtxgAzx33dOmkGBrlKHROK8u7w7v7tz1jjRjOMYf2JiMVII5a6U_Ga99muEZlykMxejVkTISSuowaVgbraHMZEbSUtAp5KvGtTxlfsiRaoAT7hy3DYW6tEHx7P8wPRU5E5My1sNoPKxX5o9_zdheeKErni1LDK1ax1aasCg9BIWjWFCk3lqdaHYrZbagt2mE3ra6XaD5o_aH88O3R8ymnLvTpmg8jz1d0joNBE2kRxRhzpds6ouATKFNQB2TgLkLFW5oRZ8ETk5C9DrBFeewF8CBLLMHLiUxzgOk-uL3z43qAQwVNfaZ7MZqc9-0ZR1v7CvVeyflXmzNj4sQlVgCC2ytHkvt8nNCM-cM3kchpbn7UTqgglFbdlOdBZqYTXTcqpD4fUOCyM1ZeEiurzqkqhzEXXCH4_3j-PeuPfQH9z9HM9LUhrXzhfpCDdR6m69WUfXpErE_xaCL7_qFUQniIDI6LK7fWEqpATLRIKgW7RRCmXUpsL3WMfLewEFaj1vEaGwljcEfd8p9BRErOt3SV6Aw2borgjZc_fUYJhoybYWmICqoattbOEm-y25FIp_fyaP_qJhiJYz4Xjf0MSTG8vPdycHmIQaHpn5etrurV7u0u21yAjXLTLCxzrsiFVyB4BPRqgUIp6TNrmOeNY60mK_NEM9kUOg3WL8bzk-bBVrpK3nel_bgYZz5i1536fRngrRkc3DwMyrvfsBQrsCNHccYG38MZnjeI2rYxvH14QEuJ4UHF_4B8YnRYpjZwseYJ7pJzEojf8muGX1BDRenLtswlntttn8AQ
 // v2: https://og-playground.vercel.app/?share=nVb_T-IwFP9X6hIDJoNtnJxIzkuM53lE70zUxF9IsGyPUe3W2XZ8kfC_3-sGCBvTyz3CRvs-ff207xsLyxcBWF2LfAvYpB-TjSg953C2WGzPETIGFo51l9Q81z2s2bvKKQv0uEIXMJVwOkftiMOsqNUw0-echTHqfYg1yCIiEYppJgxAAqeaTaAIocZAT0OkVrs0lKZSF2HPqdJsNL8QuE1sjlINNZofTIK_2tkXPI3ifahHSRMExGKKP0oAxvFEXdK3FNWppBrqXrN91LcKuCH1X0Ip0ji4EFzggpoMh7Tearft9Zc0T4-K5rcX3rM3qPDANqwX0dDgUsnrY60T1XUcCarpc5EGLKZy3vRF5AgmRSD4SDjMLHDShAsaOBPv60nb63TarusoiNgIV_BB_MZfZdBM4rDE0HFKe5cABE9LA0Z5IzRvdE3dZ9LnQKgmrXYyyx42SVKZ4GTr0CZa0lglVCKWuIdHNqk2cGIMmIdNMErGGiHzfTY-ZP5-uWgse9a20cvl--j7tqKYWlXJ9VmW_HOIfxzku9ht1gXeyJxFYdG0kv5Z7T-DxvVax17Lubm9uh1cD6579w-XfwYvc3ee6KaahAVq63JztvA67g7N7TskEZUhM-nZMn45RifXCqdCP-46xEGP4Ez-2bFa9tVH_solJ3ADI7zoL27JF5tIynF3eQU9cauQEZ095pW0bx13DktlYi3bRZPj5uUoyOWzoMplhKGSB7hXeQaD-UkjxtHY03msRWyTWvYmdxCmnGLZJrX7hPpArqTQoF5womfquU0UJlpDgWSjpwrzHDQizXIWh0i34TbdNkRVjPFGM9LGN1lbWDHYj8banZXU6ZjpUuvYMGAx_Fq3uNN9NXSzdWYnOypCEwmNfZXfSDEUCylm5Dek2OAgooZAIMgrSYHgHI2ETWKhFD04OChk5iqE32d2g3l3lKMt2xKJaWXK6i6srFtbXe-05dpWnmg4cjs4CmCYhlZ3RLkC24JIPLOHeWL-JuhpNkJL5uovoyEEVlfLFJbLvw
 // v3 https://og-playground.vercel.app/?share=nVb_T-IwFP9X6hIDJoNtnJxIzkuM53lE70zUxF9IsGyPUe3W2XZ8kfC_3-sGCBvTyz3CRvs-ff207xsLyxcBWF2LfAvYpB-TjSg953C2WGzPETIGFo51l9Q81z2s2bvKKQv0uEIXMJVwOkftiMOsqNUw0-echTHqfYg1yCIiEYppJgxAAqeaTaAIocZAT0OkVrs0lKZSF2HPqdJsNL8QuE1sjlINNZofTIK_2tkXPI3ifahHSRMExGKKP0oAxvFEXdK3FNWppBrqXrN91LcKuCH1X0Ip0ji4EFzggpoMh7Tearft9Zc0T4-K5rcX3rM3qPDANqwX0dDgUsnrY60T1XUcCarpc5EGLKZy3vRF5AgmRSD4SDjMLHDShAsaOBPv60nb63TarusoiNgIV_BB_MZfZdBM4rDE0HFKe5cABE9LA0Z5IzRvdE3dZ9LnQKgmrXYyyx42SVKZ4GTr0CZa0lglVCKWuIdHNqk2cGIMmIdNMErGGiHzfTY-ZP5-uWgse9a20cvl--j7tqKYWlXJ9VmW_HOIfxzku9ht1gXeyJxFYdG0kv5Z7T-DxvVax17Lubm9uh1cD6579w-XfwYvc3ee6KaahAVq63JztvA67g7N7TskEZUhM-nZMn45RifXCqdCP-46xEGP4Ez-2bFa9tVH_solJ3ADI7zoL27JF5tIynF3eQU9cauQEZ095pW0bx13DktlYi3bRZPj5uUoyOWzoMplhKGSB7hXeQaD-UkjxtHY03msRWyTWvYmdxCmnGLZJrX7hPpArqTQoF5womfquU0UJlpDgWSjpwrzHDQizXIWh0i34TbdNkRVjPFGM9LGN1lbWDHYj8banZXU6ZjpUuvYMGAx_Fq3uNN9NXSzdWYnOypCEwmNfZXfSDEUCylm5Dek2OAgooZAIMgrSYHgHI2ETWKhFD04OChk5iqE32d2g3l3lKMt2xKJaWXK6i6srFtbXe-05dpWnmg4cjs4CmCYhlZ3RLkC24JIPLOHeWL-JuhpNkJL5uovoyEEVlfLFJbLvw
-import { ImageResponse } from '@vercel/og'
-import { NextRequest } from 'next/server'
+import {ImageResponse} from '@vercel/og'
+import {NextRequest} from 'next/server'
 
 export const config = {
-  runtime: 'edge'
+    runtime: 'edge'
 }
 
 const font = fetch(
@@ -14,111 +14,110 @@ const font = fetch(
 
 
 export default async function handler(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url)
+    try {
+        const {searchParams} = new URL(req.url)
 
-    // ?title=<title>
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
-      : 'rodkisten.com'
+        // ?title=<title>
+        const hasTitle = searchParams.has('title')
+        const title = hasTitle
+            ? searchParams.get('title')?.slice(0, 100)
+            : 'rodkisten.com'
 
-      const fontData = await font;
-     let image = null
+        const fontData = await font;
+        let image = null
 
-    if(searchParams.has('image')) {
-        image = searchParams.get('image')
+        if (searchParams.has('image')) {
+            image = searchParams.get('image')
+        }
+
+
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                        display: 'flex',
+                        textAlign: 'center',
+                        position: 'relative',
+                        alignItems: 'flex-start',
+                        justifyContent: 'flex-start',
+                        flexDirection: 'column',
+                        flexWrap: 'nowrap',
+                        filter: "saturate(1.5)",
+                        backgroundColor: 'rgba(255,255,255, .9)',
+                        backgroundSize: '100%',
+                        backgroundImage: 'url(https://res.cloudinary.com/oirodolfo/image/upload/v1675188500/semifinal_nzlqrd.png)',
+                        // backgroundImage:
+                        //   'radial-gradient(circle at 25px 25px, purple 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+                        // backgroundSize: '100px 100px'
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'flex-start',
+                            justifyContent: 'flex-start'
+                        }}
+                    >
+                        <img
+                            src='https://res.cloudinary.com/oirodolfo/image/upload/v1675012412/LOGO_K_KISTEN_ky0ypt.svg'
+                            height={180}
+                            style={{margin: '20px 45px'}}
+                        />
+                    </div>
+
+
+                    {title &&
+                      <div
+                        style={{
+                            marginLeft: 30,
+                            // marginRight: 700,
+                            maxWidth: "48%",
+                            textAlign: 'left',
+                            display: 'flex',
+                            fontSize: 130,
+                            fontFamily: `Anton, 'Anton Regular', 'Space Grotesk', Inter, sans-serif`,
+                            letterSpacing: '-0.05em',
+                            // fontStyle: 'regular',
+                            color: 'white',
+                            lineHeight: '90%',
+                            // whiteSpace: 'pre-wrap',
+                        }}
+                      >
+                          {title}
+                      </div>}
+
+
+                    <img
+                        src={image || 'https://images.unsplash.com/photo-1496449903678-68ddcb189a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'}
+                        style={{
+                            width: '761px',
+                            position: 'absolute',
+                            height: '761px',
+                            left: '1007px',
+                            top: '156px'
+                        }}/>
+
+
+                </div>
+            ),
+            {
+                width: 1920,
+                height: 1080,
+                fonts: [
+                    {
+                        name: 'Anton',
+                        data: fontData,
+                        style: 'normal',
+                    },
+                ],
+            }
+        )
+    } catch (e: any) {
+        console.log(`${e.message}`)
+        return new Response('Failed to generate the image', {
+            status: 500
+        })
     }
-
-
-    return new ImageResponse(
-      (
-          <div
-              style={{
-                  height: '100%',
-                  width: '100%',
-                  display: 'flex',
-                  textAlign: 'center',
-                  position: 'relative',
-                  alignItems: 'flex-start',
-                  justifyContent: 'flex-start',
-                  flexDirection: 'column',
-                  flexWrap: 'nowrap',
-                  filter: "saturate(1.5)",
-                  backgroundColor: 'rgba(255,255,255, .9)',
-                  backgroundSize: '100%',
-                  backgroundImage: 'url(https://res.cloudinary.com/oirodolfo/image/upload/v1675188500/semifinal_nzlqrd.png)',
-                  // backgroundImage:
-                  //   'radial-gradient(circle at 25px 25px, purple 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
-                  // backgroundSize: '100px 100px'
-              }}
-          >
-              <div
-                  style={{
-                      display: 'flex',
-                      alignItems: 'flex-start',
-                      justifyContent: 'flex-start'
-                  }}
-              >
-                  <img
-                      src='https://res.cloudinary.com/oirodolfo/image/upload/v1675012412/LOGO_K_KISTEN_ky0ypt.svg'
-                      height={180}
-                      style={{ margin: '20px 45px' }}
-                  />
-              </div>
-
-
-
-              {title &&
-              <div
-                  style={{
-                      marginLeft: 30,
-                      // marginRight: 700,
-                      maxWidth: "48%",
-                      textAlign: 'left',
-                      display: 'flex',
-                      fontSize: 130,
-                      fontFamily: `Anton, 'Anton Regular', 'Space Grotesk', Inter, sans-serif`,
-                      letterSpacing: '-0.05em',
-                      // fontStyle: 'regular',
-                      color: 'white',
-                      lineHeight: '90%',
-                      // whiteSpace: 'pre-wrap',
-                  }}
-              >
-                  {title}
-              </div>}
-
-
-
-
-              <img src={ image || 'https://images.unsplash.com/photo-1496449903678-68ddcb189a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
-                   style={{ width: '761px',
-                       position: 'absolute',
-                       height: '761px',
-                       left: '1007px',
-                       top: '156px'}} />
-
-
-
-          </div>
-      ),
-      {
-        width: 1920,
-        height: 1080,
-          fonts: [
-              {
-                  name: 'Anton',
-                  data: fontData,
-                  style: 'normal',
-              },
-          ],
-      }
-    )
-  } catch (e: any) {
-    console.log(`${e.message}`)
-    return new Response('Failed to generate the image', {
-      status: 500
-    })
-  }
 }
