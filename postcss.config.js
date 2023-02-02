d8f4f8f57a2cc5a0061ab88dd61a8694d4d513c5
Rod Kisten <rod@Rods-MacBook-Pro.local>
2023-02-02 19:56:17 -0300

    colors

--- postcss.config.js
+++ postcss.config.js
@@ -36,6 +36,8 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: { calc: false} } : {})
+
   }
 }
